25fed1b8af069a95b9149e21e486aeae6fbf677c
sultan93291 <130165694+sultan93291@users.noreply.github.com>
2023-12-30 12:39:50 +0600

    chruch section done

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,8 @@ module.exports = {
       colors: {
         black: "rgba(28, 29, 40, 1)",
         text_color: "rgba(255, 255, 255, 1)",
-        overlay: "rgba(196, 196, 196, 0)",
+        overlay:
+          "linear-gradient(110deg, #CD5423 -53.83%, #CD5423 -53.82%, #FFD0A0 62.61%)",
         btn_color: "#FFD2A4",
         unique_txt: "#DC9853",
       },
