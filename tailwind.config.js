d201d6d0b29f983d4c93c6b4443af1c0cefb4fbc
sultan93291 <130165694+sultan93291@users.noreply.github.com>
2023-12-30 22:14:03 +0600

    ready for production without responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,7 @@ module.exports = {
       colors: {
         black: "rgba(28, 29, 40, 1)",
         text_color: "rgba(255, 255, 255, 1)",
-        overlay:
-          "linear-gradient(110deg, #CD5423 -53.83%, #CD5423 -53.82%, #FFD0A0 62.61%)",
+        overlay:"rgba(0,0,0,0.4)",
         btn_color: "#FFD2A4",
         unique_txt: "#DC9853",
       },
